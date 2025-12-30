0a1f626200e12ca72225101ee1ca36359f3554ae
lucasmonsan <lucasmonsan@gmail.com>
2025-12-30 12:02:03 -0300

    Add styled system files

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,12 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$routes: 'src/routes'
+		}
+	}
 };
 
 export default config;
